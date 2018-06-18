10522dfbd27378fe699f90935f63e63f0e1ae187
Jeremy <jeremyzehr@lspadmins-MacBook-Pro-2.local>
2018-06-17 20:35:25 -0400

    Sample trial: Rating scale

---
+++ data_includes/example_data.js
@@ -0,0 +1,16 @@
+var shuffleSequence = seq("trial");
+PennController.ResetPrefix(null);
+
+var items = [
+    ["trial", "PennController", PennController(
+        newText("test sentence", "The mouse with which the cat that the dog chases plays moves fast.")
+            .print()
+        ,
+        newText("question", "How easy do you find it to process the sentence above?")
+        ,
+        // We create a 5-point scale
+        newScale("answer", 5)
+            .print()
+            .wait() // This waits for a selection before validation
+    )]
+];
